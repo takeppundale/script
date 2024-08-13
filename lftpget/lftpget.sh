a4f7b5432a9158f8d80b4965b5bdb6700ecde719
t.watada <t.watada@gmail.com>
2024-08-14 08:35:27 +0900

    一旦、コミット

--- lftpget/lftpget.sh
+++ lftpget/lftpget.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 SID=XXXXXX
 USER=XXXXXX
@@ -9,15 +9,14 @@ SLEEP_TIME=500		# msec
 
 while [ $DOROOP=yes ]
 do
-	DATE_FROM=$(date --date "12 minutes ago 30 seconds" "+%Y%m%d%H:%M" | awk -F: '{ $2 = int($2 / 10) * 10; if ($2 == 0) {$2 = "00"} print $1$2 0 0}')
-	DATE_TO=$(date --date "2 minutes ago 30 seconds" "+%Y%m%d%H:%M" | awk -F: '{ $2 = int($2 / 10) * 10; if ($2 == 0) {$2 = "00"} print $1$2 0 0}')
-#	echo ${DATE_TO}
-#	echo ${DATE_FROM}
+    DATE_FROM=$(date --date "12 minutes ago 30 seconds" "+%Y%m%d%H:%M" | awk -F: '{ $2 = int($2 / 10) * 10; if ($2 == 0) {$2 = "00"} print $1$2 0 0}')
+    DATE_TO=$(date --date "2 minutes ago 30 seconds" "+%Y%m%d%H:%M" | awk -F: '{ $2 = int($2 / 10) * 10; if ($2 == 0) {$2 = "00"} print $1$2 0 0}')
+    # echo ${DATE_TO}
+    # echo ${DATE_FROM}
 
-	FILE_NAME=${SID}_${DATE_FROM}_${DATE_TO}_1.csv
-#	echo ${FILE_NAME}
-	lftp -u $USER,$PASS $SERVER -e "get report/$FILE_NAME; bye"
+    FILE_NAME=${SID}_${DATE_FROM}_${DATE_TO}_1.csv
+    # echo ${FILE_NAME}
+    lftp -u $USER,$PASS $SERVER -e "get report/$FILE_NAME; bye"
 
-	usleep ${SLEEP_TIME}
+    usleep ${SLEEP_TIME}
 done
-
